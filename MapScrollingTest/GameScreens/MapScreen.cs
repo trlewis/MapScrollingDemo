632a2982e0171ad4ad2b7ccc9f4c9513e14dd8a5
Travis L <econobeing5@gmail.com>
2017-04-18 19:11:44 -0700

    initial commit

---
+++ MapScrollingTest/GameScreens/MapScreen.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapScrollingTest.GameScreens
+{
+    using SFML.Graphics;
+    using SFML.System;
+
+    public class MapScreen
+    {
+        private const int TileSize = 32;
+        private List<RectangleShape> tiles = new List<RectangleShape>();
+
+        public MapScreen()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                var rekt = new RectangleShape();
+                rekt.Position = GetRandomTilePosition();
+                rekt.Size = new Vector2f(TileSize, TileSize);
+                rekt.FillColor = Color.Green;
+                this.tiles.Add(rekt);
+            }
+        }
+
+        public void Draw(RenderTarget target)
+        {
+            foreach(var tile in this.tiles)
+                target.Draw(tile);
+        }
+
+        private Vector2f GetRandomTilePosition()
+        {
+            Random rand = new Random();
+            int x = rand.Next(640 / TileSize) * TileSize;
+            int y = rand.Next(480 / TileSize) * TileSize;
+            return new Vector2f(x, y);
+        }
+    }
+}
---
+++ MapScrollingTest/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapScrollingTest
+{
+    using MapScrollingTest.GameScreens;
+    using SFML.Graphics;
+    using SFML.Window;
+
+    class Program
+    {
+        private static RenderWindow window;
+        private static MapScreen map;
+
+        static void Main(string[] args)
+        {
+            Initialize();
+            MainLoop();
+            //Console.ReadKey(); //can't readkey if there is no console.
+        }
+
+        private static void MainLoop()
+        {
+            while(window.IsOpen)
+            {
+                window.DispatchEvents();
+                window.Clear(Color.Black);
+                map.Draw(window);
+                window.Display();
+            }
+            Console.WriteLine("main loop ended");
+        }
+
+        private static void Initialize()
+        {
+            var contextSettings = new ContextSettings { DepthBits = 24 };
+            var vidMode = new VideoMode(640, 480);
+            window = new RenderWindow(vidMode, "Map Scroller", Styles.Close | Styles.Titlebar, contextSettings);
+            window.SetActive();
+            window.SetKeyRepeatEnabled(false);
+            window.Closed += (sender, args) => ShutDown();
+
+            map = new MapScreen();
+        }
+
+        private static void ShutDown()
+        {
+            window.SetActive(false);
+            window.Close();
+        }
+    }
+}
